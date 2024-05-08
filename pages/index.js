50d4bb9bb55862a5941153fbbe9d14135ddaddf9
Nina Weber <weber_nina@web.de>
2024-05-08 11:44:28 +0200

    add search inputfield / index.js

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,21 @@
 import styled from "styled-components";
+import { useState } from "react";
 import BookList from "@/components/BookList";
 
 export default function HomePage({ books }) {
+  const [searchTerm, setSearchTerm] = useState("");
+
   return (
     <>
       <header>
         <h1>Book Hunting</h1>
       </header>
       <main>
+        <input
+          type="text"
+          name="search"
+          onChange={(event) => setSearchTerm(event.target.value)}
+        ></input>
         <BookList books={books} />
       </main>
     </>
